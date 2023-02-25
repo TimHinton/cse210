166b47e86303a11cb75fe82647d6f055918f0066
TimHinton <timjhinton@gmail.com>
2023-02-24 23:57:35 -0700

    completed 2 and 3

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,79 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int int_perc = int.Parse(percentage);
+        string grade;
+        string add = "";
+
+        if (int_perc >= 90)
+        {
+            grade = "A";
+            if (int_perc < 93)
+            {
+                add = "-";
+            }
+        }
+
+        else if (int_perc >= 80)
+        {
+            grade = "B";
+            if (int_perc >= 97)
+            {
+                add = "+";
+            }
+            else if (int_perc < 93)
+            {
+                add = "-";
+            }
+        }
+
+        else if (int_perc >= 70)
+        {
+            grade = "C";
+            if (int_perc >= 97)
+            {
+                add = "+";
+            }
+            else if (int_perc < 93)
+            {
+                add = "-";
+            }
+        }
+
+        else if (int_perc >= 60)
+        {
+            grade = "D";
+            if (int_perc >= 97)
+            {
+                add = "+";
+            }
+            else if (int_perc < 93)
+            {
+                add = "-";
+            }
+        }
+
+        else
+        {
+            grade = "F";
+            if (int_perc >= 97)
+            {
+                add = "+";
+            }
+        }
+
+        Console.WriteLine($"Grade: {grade}{add}");
+
+        if (int_perc >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you needed at least a 70% to pass the class.");
+        }
+
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string continu = "yes";
+
+
+        // Do loop
+        while (continu == "yes")
+        {
+            int int_guess = 0;
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 10);
+            Console.Write("I have chosen a magic number.");
+            
+            while (int_guess != number)
+            {
+                Console.Write("What is your guess?");
+                string guess = Console.ReadLine();
+
+                int_guess = int.Parse(guess);
+
+                if (int_guess == number)
+                {
+                    Console.WriteLine("Great work! You guessed the magic number.");
+                    break;
+                }
+
+                else if (int_guess > number)
+                {
+                    Console.WriteLine("Lower...");
+                }
+
+                else
+                {
+                    Console.WriteLine("Higher...");
+                }
+            }
+
+        Console.Write("Would you like to play again (yes/no) ? ");
+        continu = Console.ReadLine();
+
+        } while (continu == "yes");
+
     }
 }
